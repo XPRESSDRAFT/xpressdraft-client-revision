6ff857b35b45216bba11368cff46a079df70fc66
XPRESSDRAFT PTY LTD <info@xpressdraft.com.au>
2026-07-28 17:37:55 +1000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";// v2
 import { BrowserRouter, Routes, Route, useNavigate, useSearchParams } from "react-router-dom";
 import * as api from "./api";
 import DrawingView from "./DrawingView";
